94ca2cd4e61f754ac7fa451a554f9b1ee73b2eb0
stephen smith ecs <wearypossum4770@yahoo.com>
2022-11-11 11:26:47 -0500

    add subresource-integirty hasher

---
+++ subresource_integrity/constants.js
@@ -0,0 +1,29 @@
+export const resolveAlgorighm = (value) =>
+  Object.freeze(
+    new Map([
+      ["sha1", "SHA-1"],
+      [1, "SHA-1"],
+      ["1", "SHA-1"],
+      ["SHA1", "SHA-1"],
+      ["sha256", "SHA-256"],
+      [256, "SHA-256"],
+      ["256", "SHA-256"],
+      ["SHA-26", "SHA-256"],
+      ["sha384", "SHA-384"],
+      [384, "SHA-384"],
+      ["384", "SHA-384"],
+      ["SHA-34", "SHA-384"],
+      ["sha512", "SHA-512"],
+      [512, "SHA-512"],
+      ["512", "SHA-512"],
+      ["SHA-52", "SHA-512"],
+    ])
+  );
+export const algorithmWarning =
+  "For increased security valid algorithmic options are one of the following. SHA-1 | SHA-256 | SHA-384 | SHA-512";
+export const crossOriginWarning =
+  "Setting the cross origin to a value other than anonymous can create a security risk.";
+export const resolveCrossOrigin = (value) =>
+  Object.freeze(new Set(["anonymous", "use-credentials"])).has(
+    value.toLowerCase()
+  );
---
+++ subresource_integrity/index.js
@@ -0,0 +1,56 @@
+import crypto from "crypto";
+import { writeFileSync } from "fs";
+import {
+  resolveAlgorighm,
+  algorithmWarning,
+  crossOriginWarning,
+  resolveCrossOrigin,
+} from "./constants";
+import htmlHandler from "./utils";
+
+const { version } = process;
+const defaultAlgorithm = "SHA-256";
+
+export default function generateIntegrityHash(config = {}) {
+  const { algorithm, crossorigin } = config;
+  const prefix = algorithm
+    ? algorithm.replaceAll(/\-/g, "").toLowerCase()
+    : "sha256";
+  return {
+    name: "generate-integirty-hash",
+    defaultCrossorigin: "anonymous",
+    async generateBundle(_outputOptions, assetInfo) {
+      const contents = [];
+      if (!crossorigin)
+        this.warn(
+          "crossorigin value not set, default 'anonymous' will be used."
+        );
+      else if (crossorigin !== "anonymous") this.warn(crossOriginWarning);
+      if (!resolveAlgorighm(algorithm)) this.warn(algorithmWarning);
+      const assets = Object.keys(assetInfo);
+      for (let item in assets) {
+        const filename = assets[item];
+        const context = assetInfo[filename];
+        if (filename.endsWith("css")) {
+          const data = context.source ?? context.code;
+          const hashBuffer = await crypto.webcrypto.subtle.digest(
+            algorithm ?? defaultAlgorithm,
+            new Buffer.from(data)
+          );
+          const hashArray = new Buffer.from(new Uint8Array(hashBuffer));
+          const integrity = `${prefix}-${hashArray.toString("base64")}`;
+          contents.push({
+            filename,
+            integrity,
+            version,
+            build_date: new Date().toISOString(),
+          });
+        }
+        if (filename.endsWith(".html")) {
+          this.emitFile({ ...context, source: htmlHandler(context) });
+        }
+      }
+      writeFileSync("rollup_subresource_cache.json", JSON.stringify(contents));
+    },
+  };
+}
