1363f0aa40343ae7efb76cbf178f3a9197f04a80
Dev Steve <wearypossum4770@yahoo.com>
2022-11-16 18:53:50 -0500

    format modules

--- src/analytics/lib.rs
+++ src/analytics/lib.rs
@@ -1,22 +1,21 @@
-use serde::{Deserialize, Serialize};
 use crate::AppState;
+use serde::{Deserialize, Serialize};
 #[derive(Debug, Default, Deserialize, Serialize)]
-pub struct Analytics {  
-  pub source: String,
-  pub first_name: String,
-  pub last_name: String,
-  pub email: String,
-  pub age: u8,
+pub struct Analytics {
+    pub source: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub age: u8,
 }
 
-
 impl Analytics {
- pub fn new() -> Analytics  {
-  Analytics {
-    ..Default::default()
-  }
- }
- pub fn create_table(conn: &AppState) {
-  conn.create_table("identity()");
- }
+    pub fn new() -> Analytics {
+        Analytics {
+            ..Default::default()
+        }
+    }
+    pub fn create_table(conn: &AppState) {
+        conn.create_table("identity()");
+    }
 }
