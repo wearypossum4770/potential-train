5472654fbdfaf90a4e5772c68848e8720bd403ff
Dev Steve <wearypossum4770@yahoo.com>
2022-11-23 20:37:15 -0500

    move subresource integrity package

---
+++ scripts/subresource_integrity/index.js
@@ -0,0 +1,59 @@
+import crypto from "crypto";
+import { writeFileSync } from "fs";
+import { resolve } from 'path'
+import {
+  resolveAlgorighm,
+  algorithmWarning,
+  crossOriginWarning,
+  resolveCrossOrigin,
+} from "./constants";
+import htmlHandler from "./utils";
+
+const { version } = process;
+const defaultAlgorithm = "SHA-256";
+
+export default function generateIntegrityHash(config = {}) {
+  const { algorithm, crossorigin } = config;
+  const prefix = algorithm
+    ? algorithm.replaceAll(/\-/g, "").toLowerCase()
+    : "sha256";
+  return {
+    name: "generate-integirty-hash",
+    defaultCrossorigin: "anonymous",
+    async generateBundle(_outputOptions, assetInfo) {
+      const contents = [];
+      if (!crossorigin)
+        this.warn(
+          "crossorigin value not set, default 'anonymous' will be used."
+        );
+      else if (crossorigin !== "anonymous") this.warn(crossOriginWarning);
+      if (!resolveAlgorighm(algorithm)) this.warn(algorithmWarning);
+      const assets = Object.keys(assetInfo);
+      for (let item in assets) {
+        const filename = assets[item];
+        const context = assetInfo[filename];
+        if (filename.endsWith("css")) {
+          const data = context.source ?? context.code;
+          const hashBuffer = await crypto.webcrypto.subtle.digest(
+            algorithm ?? defaultAlgorithm,
+            new Buffer.from(data)
+          );
+          const hashArray = new Buffer.from(new Uint8Array(hashBuffer));
+          const integrity = `${prefix}-${hashArray.toString("base64")}`;
+          contents.push({
+            filename,
+            integrity,
+            version,
+            build_date: new Date().toISOString(),
+          });
+        }
+        if (filename.endsWith(".html")) {
+          const source = htmlHandler({ ...config, source: context.source })
+          const { name, type, fileName } = context;
+          this.emitFile({ source, type, name, fileName });
+        }
+      }
+      writeFileSync(resolve('./scripts/rollup_subresource_cache.json'), JSON.stringify(contents));
+    },
+  };
+}
---
+++ scripts/subresource_integrity/utils.js
@@ -0,0 +1,47 @@
+import * as cheerio from "cheerio";
+import { readFileSync } from "fs";
+import { resolve } from 'path'
+const transformLink = (attrs) => ({
+  ...attrs,
+  title: "English HTML",
+  rel: "preload",
+});
+export default function htmlHandler({
+  source,
+  asAsync = false,
+  asDefer = false,
+  usBlocking = false,
+  crossorigin = "",
+  isModule = false,
+  referrerpolicy = "",
+  href = "",
+  cssPreload = false,
+  title = "",
+  transformLink: fn,
+}) {
+  const data = JSON.parse(
+    readFileSync(resolve('./scripts/rollup_subresource_cache.json'), "utf-8") 
+  );
+  const $ = cheerio.load(source);
+  const links = $("link");
+  const scripts = $("script");
+  for (let i = 0; i < scripts.length; i++) {
+    const script = scripts[i];
+    const { src } = script.attribs;
+    const element = data.filter((ctx) => ctx.filename === src.slice(1))[0];
+    if (element) {
+      const { integrity } = element;
+      Object.assign(script.attribs, { integrity, crossorigin: "anonymous" });
+    }
+  }
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+    const { href } = link.attribs;
+    const element = data.filter((ctx) => ctx.filename === href.slice(1))[0];
+    if (element) {
+      const { integrity } = element;
+      Object.assign(link.attribs, { integrity, crossorigin: "anonymous" });
+    }
+  }
+  return $.html();
+}
